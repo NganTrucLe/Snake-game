9dbf0fdbda735f695d322d0173ba509b8cfa8d41
ngantruc2003 <ngantruc2003@gmail.com>
2022-04-16 00:18:09 +0700

    finish

--- SaveAndLoad.h
+++ SaveAndLoad.h
@@ -14,20 +14,18 @@ struct HIGHSCORE {
     char* time = new char[30];
 };
 
-bool CheckEmptyHighScoreFile();
-void SaveHighScore(HIGHSCORE HighScore[5]);
-void CreateNewHighScore(HIGHSCORE HighScore[5], HIGHSCORE NewScore);
-void SortHighScore(HIGHSCORE HighScore[5]);
-void InitializeHighScore(HIGHSCORE HighScore[5]);
-void ResetHighScore(HIGHSCORE HighScore[5]);
+bool checkEmptyHighScoreFile();
+void saveHighScore(HIGHSCORE HighScore[5]);
+void createNewHighScore(HIGHSCORE HighScore[5], HIGHSCORE NewScore);
+void sortHighScore(HIGHSCORE HighScore[5]);
+void initializeHighScore(HIGHSCORE HighScore[5]);
+void resetHighScore(HIGHSCORE HighScore[5]);
 void saveDataGame(char FileName[], const int level, const int score, vector<pii> gate);
 void saveDataSnake(char FileName[], vector<pii>position, vector<bool>appear, const int direction);
 void saveDataFruit(char FileName[], pii position);
 void loadDataGame(char FileName[], int& level, int& score, vector<pii>& gate, vector<pii>& position, vector<bool>& appear, int& direction, pii& fruit);
 void loadDataSnake(char FileName[], vector<pii>& position, vector<bool>appear, int& direction);
 void loadDataFruit(char FileName[], pii& position);
-bool IsExistedFileName(char FileName[]);
-bool IsValidFileName(char FileName[]);
-void SaveData();
-void LoadData();
+bool isExistedFileName(char FileName[]);
+bool isValidFileName(char FileName[]);
 
